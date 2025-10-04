7c851361a0a0796868cd223b030d0cb14ca052f1
Rudzani Mudau <ndaedzomu101@gmail.com>
2025-10-04 18:15:25 +0200

    exp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	// export static HTML to the `out` folder
+	output: "export",
+	distDir: "out",
 };
 
 export default nextConfig;
